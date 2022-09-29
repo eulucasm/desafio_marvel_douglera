1edf5ba191f25cad05de73e1783006b50c3e069e
Lucas <Lucas.Marques-ext@ab-Inbev.com>
2022-09-28 22:17:51 -0300

    finalização de config da navegação

--- app/src/main/java/br/com/lucaopoletis/marveapp/ui/MainActivity.kt
+++ app/src/main/java/br/com/lucaopoletis/marveapp/ui/MainActivity.kt
@@ -2,15 +2,32 @@ package br.com.lucaopoletis.marveapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import br.com.lucaopoletis.marveapp.R
 import br.com.lucaopoletis.marveapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navHostFragment: NavHostFragment
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initViews(binding)
+    }
+
+    private fun initViews(binding: ActivityMainBinding) {
+
+        navHostFragment  = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottonNavigation.apply {
+            setupWithNavController(navController)
+            //setOnNavigationItemReselectedListener {  }
+        }
     }
 }
